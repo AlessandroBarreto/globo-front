181980b39c81f2be00a1cb561f9d19014f015a77
Alessandro Barreto <alessandrobarreto1201@gmail.com>
2021-10-26 19:28:06 -0300

    feature: box component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,12 +11,14 @@ const Home = () => {
     <Container maxWidth="md" disableGutters>
       <div className="home-container">
         <Header />
-        <Grid container >
-          <Grid item sm={6} >
-            {API_DATA.map((info) => {
-              return <Box key={info.id} text={info.text} tagName={info.tags} />;
-            })}
-          </Grid>
+        <Grid container spacing={1} classes={{ container: "grid-container" }}>
+          {API_DATA.map((info) => {
+            return (
+              <Grid item sm={6}>
+                <Box key={info.id} text={info.text} tagName={info.tags} />
+              </Grid>
+            );
+          })}
         </Grid>
       </div>
     </Container>
