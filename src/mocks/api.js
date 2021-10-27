f5c710b47cf1832478b77e135918aa82d06e1a80
Alessandro Barreto <alessandrobarreto1201@gmail.com>
2021-10-27 15:00:01 -0300

    fix: grid item same size with differents texts

--- src/mocks/api.js
+++ src/mocks/api.js
@@ -5,12 +5,12 @@ const API_DATA = [
     tags: ["batata", "cenoura", "pera"],
   },
   {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat at risus id venenatis. In a lacus nec dolor consectetur convallis. Etiam condimentum cursus odio, eget lobortis libero ultricies et. Mauris sit amet lacus ac risus luctus finibus. Nullam at lacus malesuada purus feugiat ullamcorper. Mauris id est sit amet.",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat at risus id venenatis. In a lacus nec dolor consectetur convallis. Etiam condimentum cursus odio, eget lobortis libero ultricies et. Mauris sit amet lacus ac risus luctus finibus. Nullam at lacus malesuada purus feugiat ullamcorper. Mauris id est sit amet.Mauris sit amet lacus ac risus luctus finibus. Nullam at lacus malesuada purus feugiat ullamcorper. Mauris id est sit amet.Mauris sit amet lacus ac risus luctus finibus. Nullam at lacus malesuada purus feugiat ullamcorper. Mauris id est sit amet.",
     id: 2,
     tags: ["maçã", "feijão", "arroz"],
   },
   {
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat at risus id venenatis. In a lacus nec dolor consectetur convallis. Etiam condimentum cursus odio, eget lobortis libero ultricies et. Mauris sit amet lacus ac risus luctus finibus. Nullam at lacus malesuada purus feugiat ullamcorper. Mauris id est sit amet.",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat at risus id venenatis. In a lacus nec dolor consectetur convallis. Etiam condimentum cursus odio, eget lobortis libero ultricies et. Mauris sit amet lacus ac risus luctus finibus. Nullam at lacus malesuada purus feugiat ullamcorper. Mauris id est sit ametMauris sit amet lacus ac risus luctus finibus. Nullam at lacus malesuada purus feugiat ullamcorper. Mauris id est sit amet..",
     id: 3,
     tags: ["carne", "lentilha", "frango"],
   },
