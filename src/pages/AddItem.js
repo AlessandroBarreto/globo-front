9c7d6a9110b17a1d953d263d97de92f4fbccc32b
Alessandro Barreto <alessandrobarreto1201@gmail.com>
2021-10-27 18:47:24 -0300

    feature: textField state

--- src/pages/AddItem.js
+++ src/pages/AddItem.js
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import HeaderSecondary from "../components/HeaderSecondary";
 import Container from "@mui/material/Container";
-
 import Card from "@mui/material/Card";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import "../styles/addItem.scss";
 
 const AddItem = () => {
+  const [text, setText] = useState("");
+  console.log(text);
   return (
     <div className="addItem-container">
       <HeaderSecondary />
@@ -22,6 +24,8 @@ const AddItem = () => {
             fullWidth
             color="primary"
             helperText="limite de caracteres: 400"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
           />
         </Card>
         <Button variant="contained" color="primary" fullWidth>
